68d96860a6fe4219576798c10ee5fe2d333696cd
StevesRepository <sjdamico203@sbcglobal.net>
2016-04-10 14:33:02 -0500

    Initial Commit

---
+++ ResizeModes/GameScene.swift
@@ -0,0 +1,36 @@
+//
+//  GameScene.swift
+//  ResizeModes
+//
+//  Created by Steve D'Amico on 4/7/16.
+//  Copyright (c) 2016 Steve D'Amico. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            
+            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            
+            sprite.xScale = 0.5
+            sprite.yScale = 0.5
+            sprite.position = location
+            
+            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            
+            sprite.runAction(SKAction.repeatActionForever(action))
+            
+            self.addChild(sprite)
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
